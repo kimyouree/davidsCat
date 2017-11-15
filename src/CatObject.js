0df1085c729769ce80a4c83bac1010e3583ed6df
You-Ree Kim <kimyouree@icloud.com>
2017-11-15 17:50:58 -0500

    Monday nov 12 updates

---
+++ src/CatObject.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+import './CatObject.css';
+
+class CatObject extends React.Component{
+    render(){
+        return(
+            
+                <div className="absolute" id="cat-object">
+                    <img className="cathead--size" src="/cathead.jpeg"/>
+                </div>
+            
+        )
+    }
+}
+
+
+
+
+
+
+
+export default CatObject; 
