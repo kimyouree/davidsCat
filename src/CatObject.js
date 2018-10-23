8b04a4b0e0b879d307d423217d2884b1a6a80356
kimyouree <kimyouree@icloud.com>
2018-10-23 17:42:35 -0400

    ran npm update and outdated

--- src/CatObject.js
+++ src/CatObject.js
@@ -4,11 +4,11 @@ import './CatObject.css';
 class CatObject extends React.Component{
     render(){
         return(
-            
+
                 <div className="absolute" id="cat-object">
                     <img className="" width={this.props.catW} height={this.props.catH} src="http://ultimatecat.lol/images/cat-head-tux.png"/>
                 </div>
-            
+
         )
     }
 }
@@ -19,4 +19,4 @@ class CatObject extends React.Component{
 
 
 
-export default CatObject; 
+export default CatObject;
--- src/MouseObject.js
+++ src/MouseObject.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import './MouseObject.css';
 
 class MouseObject extends React.Component{
